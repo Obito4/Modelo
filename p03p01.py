bbf8d333f544c6806da58537d98fef66fa56a4e6
Obito4 <33682870+Obito4@users.noreply.github.com>
2017-11-22 08:48:45 +0100

    Problemas tema 3

---
+++ p03p01.py
@@ -0,0 +1,8 @@
+primero=int(input("¿Cual es el primero?")) 
+segundo=int(input("¿Cual es el segundo?"))
+if primero<segundo:
+ print(primero)
+else:
+ print(segundo)
+ 
+ 
---
+++ p03p03.py
@@ -0,0 +1,19 @@
+primero=int(input("¿Cual es el primero?"))
+
+segundo=int(input("¿Cual es el segundo?"))
+ 
+tercero=int(input("¿Cual es el tercero?"))
+
+if (primero<segundo)and (primero<tercero):
+
+ print(primero)
+
+else:
+
+  if segundo>tercero:
+ 
+    print(tercero)
+
+  if tercero>segundo:
+ 
+   print(segundo)
