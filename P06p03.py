1eec64134cc64345df99b7102eb3fb59dba59871
Obito4 <33682870+Obito4@users.noreply.github.com>
2018-05-12 10:49:18 +0200

    Problema 3 Tema 6

---
+++ P06p03.py
@@ -0,0 +1,15 @@
+x = int(input("Dime los Km que quieres correr"))
+
+y= int(input("Dime los Km que debes que correr"))
+
+Dias = 0
+
+while x < y:
+
+  i = int(x) * 0.1
+
+  x = x+i
+  
+  Dias += 1
+
+print("El"+str (Dias) + " dia correre" +str(int(x+1)) + " "  +"km")
