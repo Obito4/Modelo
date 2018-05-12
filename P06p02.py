c18822e3a82a16d510df928c163ae38fe07bcc58
Obito4 <33682870+Obito4@users.noreply.github.com>
2018-05-12 10:48:16 +0200

    Problema 2 Tema 6

---
+++ P06p02.py
@@ -0,0 +1,12 @@
+N=int(input("Dime un número mayor que 2"))
+
+Divisor=2
+while True:
+
+  if N/Divisor==N//Divisor:
+
+    print(str(Divisor) +" "+ "es el menor divisor")
+
+    break
+
+  Divisor+=1
