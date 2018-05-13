21d2a03d3d3d9182c06c16da1c2541159e950db3
Obito4 <33682870+Obito4@users.noreply.github.com>
2018-05-13 13:09:38 +0200

    Problema 7 Tema 6

---
+++ P06p07.py
@@ -0,0 +1,25 @@
+numero = 0
+
+maximo = 0
+
+while True:
+
+  numero += 1
+
+  i = int(input("Dime un número"))
+
+  if i < maximo:
+ 
+   maximo = maximo
+ 
+ else:
+    
+   maximo = i
+
+ if i == 0:
+    
+   break
+
+print("El número de números recibidos es" +" "+ str(numero-1))
+
+print("El número maximo de la cadena es" +" "+ str(maximo))
