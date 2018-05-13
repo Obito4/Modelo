54978086e1f9059917d9a7bbee8db7b86217255d
Obito4 <33682870+Obito4@users.noreply.github.com>
2018-05-13 13:09:13 +0200

    Problema 6 Tema 6

---
+++ P06p06.py
@@ -0,0 +1,25 @@
+numero = 0
+
+suma = 0
+
+media = 1
+
+while True:
+
+  numero += 1
+
+  i = int(input("Dime un número"))
+
+  suma = suma + i
+
+  if i == 0:
+ 
+   break
+
+print("El número de números introducidos es" +" "+ str(numero-1))
+
+print("La suma de todos los números introducidos son" +" " +str(suma))
+
+media = suma/(numero-1)
+
+print("La media de la secuencia es" +" "+ str(media))
